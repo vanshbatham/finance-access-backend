5aa2db7b17f9f0b8baf108b4cd965b42305d31aa
vanshbatham <vanshbatham.pro@gmail.com>
2026-04-01 20:08:11 +0530

    feat: add AuthController and configure stateless Spring Security

---
+++ src/main/java/com/zorvyn/finance/controllers/AuthController.java
@@ -0,0 +1,27 @@
+package com.zorvyn.finance.controllers;
+
+import com.zorvyn.finance.dtos.UserRegistrationRequest;
+import com.zorvyn.finance.dtos.UserResponse;
+import com.zorvyn.finance.services.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<UserResponse> register(@Valid @RequestBody UserRegistrationRequest request) {
+        UserResponse response = userService.registerUser(request);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+}
